8e862e93ae465f5e91910b059cb5398a375ee670
marinarasub <d_lu333@outlook.com>
2025-03-28 04:21:16 -0700

    add cors in development

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import api from './routes/VersionRoute';
 import rateLimit from 'express-rate-limit';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+    const corsOptions = {
+        origin: 'http://localhost:5000',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    };
+    app.use(cors(corsOptions));
+}
 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
